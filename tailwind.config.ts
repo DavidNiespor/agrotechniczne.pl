6b615644b0ecb1048b73d0802dfbbb664ebaafca
DavidNiespor <d.niespor@gmail.com>
2026-01-18 12:57:40 +0100

    misc

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        agro: {
+          50: '#ecfdf5',
+          100: '#d1fae5',
+          600: '#059669',
+          700: '#047857',
+        }
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
