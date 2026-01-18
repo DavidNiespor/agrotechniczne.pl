f2bfd54951c6be42447d2a7e874a9d7c2e48ec65
DavidNiespor <d.niespor@gmail.com>
2026-01-18 16:48:02 +0100

    sa

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-// @ts-nocheck
 import { FarmManager } from '../components/FarmManager';
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -14,21 +10,5 @@ export default async function Home() {
     redirect('/login');
   }
 
-  const userData = await prisma.user.findUnique({
-    where: { email: session.user.email! },
-    include: {
-      fields: true,
-      warehouseItems: true,
-      treatments: true,
-    }
-  });
-
-  return (
-    <FarmManager 
-      initialFields={userData?.fields || []} 
-      initialWarehouse={userData?.warehouseItems || []} 
-      initialTreatments={userData?.treatments || []}
-      userEmail={session.user.email}
-    />
-  );
+  return <FarmManager />;
 }
