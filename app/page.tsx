0138808ef81737f0714487e284172b6041fe470e
DavidNiespor <d.niespor@gmail.com>
2026-01-18 18:05:17 +0100

    zmiany z dzialajacej i odpowiedniej na bez mocka

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+/*
 import { FarmManager } from '../components/FarmManager';
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
@@ -11,4 +12,40 @@ export default async function Home() {
   }
 
   return <FarmManager />;
+}
+*/
+// @ts-nocheck
+import { FarmManager } from '../components/FarmManager';
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect('/login');
+  }
+
+  // Pobieramy dane z Twojej bazy danych
+  const [fields, warehouse] = await Promise.all([
+    prisma.field.findMany({
+      where: { userId: session.user.id }
+    }),
+    prisma.warehouseItem.findMany({
+      where: { userId: session.user.id }
+    })
+  ]);
+
+  // Przekazujemy prawdziwe dane do komponentu
+  return (
+    <FarmManager 
+      session={session} 
+      initialFields={fields} 
+      initialWarehouse={warehouse} 
+    />
+  );
 }
