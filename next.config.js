af28ddb6df9407348793451fc430bc87f6d1abe0
DavidNiespor <d.niespor@gmail.com>
2026-01-24 23:50:32 +0100

    refactor: update next.config.js to enhance TypeScript and ESLint build options

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   typescript: {
+    // !! OSTRZEŻENIE !!
+    // Pozwala na build nawet jeśli masz błędy TypeScript
     ignoreBuildErrors: true,
   },
   eslint: {
+    // Pozwala na build nawet jeśli ESLint krzyczy
     ignoreDuringBuilds: true,
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
